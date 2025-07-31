64fb88286a73b4d8e7b03bc28004da1e85a8b23f
lksmeedia <lksmeedia@gmail.com>
2025-07-31 22:27:43 +0300

    Fix index.js content

--- index.js
+++ index.js
@@ -85,7 +85,4 @@ app.post('/generate-adventure', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
